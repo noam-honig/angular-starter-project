0da656f6c06fde2933d99609fec5f3836649145b
dmendezcreativ <111564830+dmendezcreativ@users.noreply.github.com>
2022-08-20 23:32:31 +0200

    Create styles-dark-mode.ts
    Implemented a dark mode, and grid wasn't usable with dark background... fixed that.

---
+++ src/styles-dark-mode.ts
@@ -0,0 +1,16 @@
+// color-adjustments for dark-mode
+.dark-mode .dataGridHeaderArea, 
+.dark-mode .dataGridFooterArea {
+  background-color: #242424 !important;
+}
+
+.dark-mode .dataGrid, 
+.dark-mode .dataGridDataCell, 
+.dark-mode .dataGridDataCell,
+.dark-mode .dataGridFooterArea {
+  border-color: #4f4f4f ! important;
+}
+
+.dark-mode .link {
+  color: #6e98ff !important;
+}
